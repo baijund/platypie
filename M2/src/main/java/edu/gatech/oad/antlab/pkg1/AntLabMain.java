3982ed6e5a9b229f97b2c5fb4ad5c07a88dc1083
shreyav <shreyav611@gmail.com>
2016-01-31 14:45:54 -0500

    Changed antlab P3

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -66,8 +66,8 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  toPrint += p2.toString("agupta400");
 		  //Person3 replace P3 with your name
 		  //and gburdell3 with your gt id
-		  Person3 p3 = new Person3("P3");
-		  toPrint += p3.toString("gburdell3");
+		  Person3 p3 = new Person3("Shreya Vissamsetti");
+		  toPrint += p3.toString("svissamsetti3");
                   //Person4 replace P4 with your name
                   //and gburdell4 with your gt id
                   Person4 p4 = new Person4("Baijun Desai");
