2438d6f8f5ac87287d18ce40fefec8de53d9ea6c
Baijun <bdesai35@gatech.edu>
2016-02-19 17:10:42 -0500

    Created nonfunctional Movies model

---
+++ NoChill/app/src/main/java/edu/gatech/cs2340/nochill/models/Movies.java
@@ -0,0 +1,16 @@
+package edu.gatech.cs2340.nochill.models;
+
+import com.android.volley.RequestQueue
+
+/**
+ * Created by Baijun on 2/19/2016.
+ */
+public class Movies {
+    private static String API_KEY = "yedukp76ffytfuy24zsqk7f5";
+
+    // Instantiate the RequestQueue.
+    RequestQueue queue = Volley.newRequestQueue(this);
+    String url ="http://www.google.com";
+
+
+}
