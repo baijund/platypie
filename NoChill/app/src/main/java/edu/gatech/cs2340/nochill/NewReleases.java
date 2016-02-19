d275b075b63ee785701b2b4eacbb4b9f4d77118a
Baijun <bdesai35@gatech.edu>
2016-02-19 18:44:10 -0500

    Created new releases activity

---
+++ NoChill/app/src/main/java/edu/gatech/cs2340/nochill/NewReleases.java
@@ -0,0 +1,14 @@
+package edu.gatech.cs2340.nochill;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class NewReleases extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_releases);
+    }
+
+}
---
+++ NoChill/app/src/main/java/edu/gatech/cs2340/nochill/models/NewReleaseRequest.java
@@ -0,0 +1,10 @@
+package edu.gatech.cs2340.nochill.models;
+
+import com.android.volley.Request;
+
+/**
+ * Created by Baijun on 2/19/2016.
+ */
+public class NewReleaseRequest extends Request<String>{
+
+}
