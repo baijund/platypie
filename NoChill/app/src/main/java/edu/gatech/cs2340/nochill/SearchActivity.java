2a76076f1e1f9d1d7e87ec29781a15d7f37dcb9d
Baijun <bdesai35@gatech.edu>
2016-02-21 00:37:06 -0500

    Did search thing

--- NoChill/app/src/main/java/edu/gatech/cs2340/nochill/SearchActivity.java
+++ NoChill/app/src/main/java/edu/gatech/cs2340/nochill/SearchActivity.java
@@ -2,6 +2,23 @@ package edu.gatech.cs2340.nochill;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.gatech.cs2340.nochill.models.Movies;
 
 public class SearchActivity extends ActionBarActivity {
 
@@ -9,6 +26,60 @@ public class SearchActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+
+        //Creates the request queue
+        Movies.initializeRequestQueue(this);
+
+        Button searchButt = ((Button) findViewById(R.id.searchButton));
+
+
+        searchButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                doQuery();
+            }
+        });
+
+    }
+
+    private void doQuery() {
+
+        EditText queryEditText = (EditText) findViewById(R.id.searchBar);
+        String q = queryEditText.getText().toString();
+        final List l = new ArrayList<MovieItem>();
+
+        Movies.query(q, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                // Display the first 500 characters of the response string.
+                Log.i("REQUEST THING", "Success");
+
+                try {
+                    JSONObject jsonRootObject = new JSONObject(response);
+                    JSONArray jsonMoviesArray = jsonRootObject.optJSONArray("movies");
+                    for (int i = 0; i < jsonMoviesArray.length(); i++) {
+                        JSONObject j = jsonMoviesArray.getJSONObject(i);
+                        Log.i("Movie : ", j.getString("title") + j.getInt("year") + j.getString("mpaa_rating"));
+
+                        l.add(new MovieItem(j.getString("title"), j.getInt("year"), j.getString("mpaa_rating")));
+                    }
+
+                } catch (JSONException e) {
+                    Log.i("Error: ", e.toString());
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i("REQUEST THING", "IT DIDNT RESPOND");
+            }
+        });
+
+        ListView lv = (ListView)findViewById(R.id.titleList);
+        lv.setAdapter(new MovieList(this, R.layout.movie_item, l));
+
     }
 
 }
--- NoChill/app/src/main/java/edu/gatech/cs2340/nochill/models/Movies.java
+++ NoChill/app/src/main/java/edu/gatech/cs2340/nochill/models/Movies.java
@@ -1,6 +1,7 @@
 package edu.gatech.cs2340.nochill.models;
 
 import android.content.Context;
+import android.net.Uri;
 import android.util.Log;
 
 import com.android.volley.Request;
@@ -9,6 +10,9 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 /**
  * Created by Baijun on 2/19/2016.
@@ -35,6 +39,17 @@ public class Movies {
 
     public static void requestInTheater(Response.Listener<String> rl, Response.ErrorListener el){
         StringRequest stringRequest = new StringRequest(Request.Method.GET, InTheatersURL, rl, el);
+        queue.add(stringRequest);
+    }
+
+    public static void query(String q, Response.Listener<String> rl, Response.ErrorListener el){
+        String url = "";
+        try{
+            url = String.format("http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=%s&q=%s", API_KEY, URLEncoder.encode(q, "UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            Log.i("Error: ", "Error encoding");
+        }
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, rl, el);
         queue.add(stringRequest);
     }
 
