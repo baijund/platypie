2238baac19b048d2a90773444af117d72041f5d9
lekhasurasani <lesurasani@gmail.com>
2016-02-27 23:32:22 -0500

    created description screens/activity

---
+++ NoChill/app/src/main/java/edu/gatech/cs2340/nochill/presenters/descriptionActivity.java
@@ -0,0 +1,16 @@
+package edu.gatech.cs2340.nochill.presenters;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import edu.gatech.cs2340.nochill.R;
+
+public class descriptionActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_description);
+    }
+
+}
