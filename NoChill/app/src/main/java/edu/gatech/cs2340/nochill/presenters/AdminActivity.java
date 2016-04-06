53f1a230c4369692da7c9fc36ee7c2bdacf2d8a6
Baijun <bdesai35@gatech.edu>
2016-04-06 00:49:12 -0400

    Renamed AdminActivity

--- NoChill/app/src/main/java/edu/gatech/cs2340/nochill/presenters/AdminActivity.java
+++ NoChill/app/src/main/java/edu/gatech/cs2340/nochill/presenters/AdminActivity.java
@@ -14,7 +14,7 @@ import edu.gatech.cs2340.nochill.R;
 import edu.gatech.cs2340.nochill.models.CurrentUser;
 import edu.gatech.cs2340.nochill.models.Users;
 
-public class minActivity extends ActionBarActivity {
+public class AdminActivity extends ActionBarActivity {
 
     private Spinner unbanSpinner;
     private ArrayAdapter<String> unbanAdapt; //Adapter displaying banned users
